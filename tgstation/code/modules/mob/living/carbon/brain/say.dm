9e519c01a3014165b70a926f15ee3b34aa0d0b47
ru.balagi@gmail.com <ru.balagi@gmail.com@0c292a2e-2435-8eda-7d57-ac5db484f014>
2011-12-07 13:02:49 +0000

    deleted /trunk

--- tgstation/code/modules/mob/living/carbon/brain/say.dm
+++
@@ -1,3 +0,0 @@
-/mob/living/carbon/brain/say(var/message)
-	if(!container) return //No container, can't speak, bucko./N
-	else	..()
--- tgstation/list_includes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-sed 's#\\#/#g' tgstation.dme | grep '^#include' | sed -r 's/^.*"(.*)".*$/\1/' | sed 's/ /\\ /g'
-
